6927ce115b5d62d4f7efe6f8affdc0669d0dd614
Tenzing Sherpa <thesherpatenzing@gmail.com>
2023-01-02 02:13:46 +0000

    Saved progress on HardcodedLogin 2023-01-02 02:13:46 +0000

--- HardcodedLogin/test-output.sh
+++ HardcodedLogin/test-output.sh
@@ -1,17 +1,12 @@
-[ERROR] COMPILATION ERROR : 
-[ERROR] /workspace/revpro-cli/java-practice/pep-labs/HardcodedLogin/src/main/java/HardcodedLogin.java:[18,21] incompatible types: java.lang.String cannot be converted to boolean
-[ERROR] /workspace/revpro-cli/java-practice/pep-labs/HardcodedLogin/src/main/java/HardcodedLogin.java:[19,25] incompatible types: java.lang.String cannot be converted to boolean
-[ERROR] /workspace/revpro-cli/java-practice/pep-labs/HardcodedLogin/src/main/java/HardcodedLogin.java:[24,27] incompatible types: java.lang.String cannot be converted to boolean
-[ERROR] /workspace/revpro-cli/java-practice/pep-labs/HardcodedLogin/src/main/java/HardcodedLogin.java:[25,25] incompatible types: java.lang.String cannot be converted to boolean
-[ERROR] Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.1:compile (default-compile) on project PEPLabsChallenges: Compilation failure: Compilation failure: 
-[ERROR] /workspace/revpro-cli/java-practice/pep-labs/HardcodedLogin/src/main/java/HardcodedLogin.java:[18,21] incompatible types: java.lang.String cannot be converted to boolean
-[ERROR] /workspace/revpro-cli/java-practice/pep-labs/HardcodedLogin/src/main/java/HardcodedLogin.java:[19,25] incompatible types: java.lang.String cannot be converted to boolean
-[ERROR] /workspace/revpro-cli/java-practice/pep-labs/HardcodedLogin/src/main/java/HardcodedLogin.java:[24,27] incompatible types: java.lang.String cannot be converted to boolean
-[ERROR] /workspace/revpro-cli/java-practice/pep-labs/HardcodedLogin/src/main/java/HardcodedLogin.java:[25,25] incompatible types: java.lang.String cannot be converted to boolean
-[ERROR] -> [Help 1]
-[ERROR] 
-[ERROR] To see the full stack trace of the errors, re-run Maven with the -e switch.
-[ERROR] Re-run Maven using the -X switch to enable full debug logging.
-[ERROR] 
-[ERROR] For more information about the errors and possible solutions, please read the following articles:
-[ERROR] [Help 1] http://cwiki.apache.org/confluence/display/MAVEN/MojoFailureException
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running HardcodedLoginTest
+Tests run: 7, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.046 sec
+
+Results :
+
+Tests run: 7, Failures: 0, Errors: 0, Skipped: 0
+
