affdb0266a2dbab4f90e60e3ecf6419933b21344
Tenzing Sherpa <thesherpatenzing@gmail.com>
2023-01-10 00:08:39 +0000

    Saved progress on PrimaryKey 2023-01-10 00:08:39 +0000

---
+++ PrimaryKey/test-output.sh
@@ -0,0 +1,12 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running CreateTableWithPKTest
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.886 sec
+
+Results :
+
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0
+
