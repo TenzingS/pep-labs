ed7f72f814b711c7453342848a2b484acb87f48a
Tenzing Sherpa <thesherpatenzing@gmail.com>
2023-01-15 16:55:24 +0000

    Saved progress on DoesNotContain 2023-01-15 16:55:24 +0000

--- DoesNotContain/src/main/java/DoesNotContain.java
+++ DoesNotContain/src/main/java/DoesNotContain.java
@@ -17,11 +17,9 @@ public class DoesNotContain {
         for(int i = 0; i < arr.length; i++){
             if(arr[i] == target){
                 return false;
-            }else {
-                return true;
             }
         }
-        return false;
+        return true;
         
     }
 }
