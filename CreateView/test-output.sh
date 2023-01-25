90bd89b7311033707198837c9b75846a72a2a471
Tenzing Sherpa <thesherpatenzing@gmail.com>
2023-01-25 22:21:28 +0000

    Saved progress on CreateView 2023-01-25 22:21:28 +0000

---
+++ CreateView/test-output.sh
@@ -0,0 +1,15 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running CreateAViewTest
+problem1: View "FIRSTNAME_LASTNAME" already exists; SQL statement:
+CREATE VIEW firstname_lastname AS SELECT firstname, lastname FROM site_user [90038-214]
+
+Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.559 sec
+
+Results :
+
+Tests run: 1, Failures: 0, Errors: 0, Skipped: 0
+
