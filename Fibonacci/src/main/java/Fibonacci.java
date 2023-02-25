24a3a7f152f46ac1ac8890e6c14d59782c31acb0
Tenzing Sherpa <thesherpatenzing@gmail.com>
2023-02-25 00:21:29 +0000

    Saved progress on Fibonacci 2023-02-25 00:21:29 +0000

--- Fibonacci/src/main/java/Fibonacci.java
+++ Fibonacci/src/main/java/Fibonacci.java
@@ -15,6 +15,14 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        int f[] = new int[n + 2];
+        int i;
+        f[0] = 0;
+        f[1] = 1;
+  
+        for(i = 2; i <= n; i++){
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        return f[n];
     }
 }
